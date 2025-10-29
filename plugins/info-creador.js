b87ba892fbe2b3fa2449767107eaa2e87d55a854
the-27 <blackoficial2025@gmail.com>
2025-10-29 18:41:13 -0500

    🍃 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 --- 𝐒𝐡𝐚𝐝𝐨𝐰𝐂𝐨𝐫𝐞 🍬

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,4 @@
 // by dv.shadow - https://github.com/Yuji-XDev
-
-import { proto } from '@whiskeysockets/baileys';
 import PhoneNumber from 'awesome-phonenumber';
 
 const handler = async (m, { conn }) => {
