8c7bd7c1809b430543439494a2d7b5cf6d4b473e
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-03 15:05:39 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -6,7 +6,7 @@ import axios from "axios"
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text?.trim())
-      return conn.reply(m.chat, `*💫 Por favor, ingresa el nombre o enlace del video.*`, m, rcanal)
+      return conn.reply(m.chat, `*▶️ Por favor, ingresa el nombre o enlace del video.*`, m, rcanal)
 
     await m.react('🎶')
 
