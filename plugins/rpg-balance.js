026a802b1f7e57ff563ec5a5fab572240a43230b
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-04 01:06:32 -0500

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -1,28 +1,16 @@
 let handler = async (m, { conn, usedPrefix }) => {
-  if (!db.data.chats[m.chat].economy && m.isGroup) {
-    return m.reply(`《✦》Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con el comando:\n» *${usedPrefix}economy on*`)
-  }
-
-  let mentionedJid = await m.mentionedJid
-  let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
-  let name = await (async () => global.db.data.users[who].name || (async () => {
-    try {
-      const n = await conn.getName(who)
-      return typeof n === 'string' && n.trim() ? n : who.split('@')[0]
-    } catch {
-      return who.split('@')[0]
-    }
-  })())()
-
-  if (!(who in global.db.data.users)) return m.reply(`ꕥ El usuario no se encuentra en mi base de datos.`)
-
-  let user = global.db.data.users[who]
-  let coin = user.coin || 0
-  let bank = user.bank || 0
-  let total = coin + bank
-  let currency = 'USD'
-
-  const texto = `ᥫ᭡ Informacion -  Balance ❀
+if (!db.data.chats[m.chat].economy && m.isGroup) {
+return m.reply(`《✦》Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con el comando:\n» *${usedPrefix}economy on*`)
+}
+let mentionedJid = await m.mentionedJid
+let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
+let name = await (async () => global.db.data.users[who].name || (async () => { try { const n = await conn.getName(who); return typeof n === 'string' && n.trim() ? n : who.split('@')[0] } catch { return who.split('@')[0] } })())()
+if (!(who in global.db.data.users)) return m.reply(`ꕥ El usuario no se encuentra en mi base de datos.`)
+let user = global.db.data.users[who]
+let coin = user.coin || 0
+let bank = user.bank || 0
+let total = (user.coin || 0) + (user.bank || 0)
+const texto = `ᥫ᭡ Informacion -  Balance ❀
  
 ᰔᩚ Usuario » *${name}*   
 ⛀ Cartera » *¥${coin.toLocaleString()} ${currency}*
