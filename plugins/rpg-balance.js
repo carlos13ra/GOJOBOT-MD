97d9cf64fe37c11854f2fbd4b772324096853cc7
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-04 00:49:05 -0500

    Update rpg-balance.js

--- plugins/rpg-balance.js
+++ plugins/rpg-balance.js
@@ -3,25 +3,6 @@ let handler = async (m, { conn, usedPrefix }) => {
     return m.reply(`《✦》Los comandos de *Economía* están desactivados en este grupo.\n\nUn *administrador* puede activarlos con el comando:\n» *${usedPrefix}economy on*`)
   }
 
-  let videos = [
-    'https://files.catbox.moe/c26j4n.mp4',
-    'https://files.catbox.moe/fazi1o.mp4',
-    'https://files.catbox.moe/bxhw5h.mp4',
-    'https://files.catbox.moe/esb1sa.mp4',
-    'https://files.catbox.moe/xthtfx.mp4',
-    'https://files.catbox.moe/70legl.mp4'                            
-  ]
-  let video = videos[Math.floor(Math.random() * videos.length)]
-
-  let mentionedJid = await m.mentionedJid
-  let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
-  let name = await (async () => global.db.data.users[who].name || (async () => {
-    try {
-      const n = await conn.getName(who)
-      return typeof n === '//files.catbox.moe/prem4p.mp4'
-  ]
-  let video = videos[Math.floor(Math.random() * videos.length)]
-
   let mentionedJid = await m.mentionedJid
   let who = mentionedJid[0] ? mentionedJid[0] : m.quoted ? await m.quoted.sender : m.sender
   let name = await (async () => global.db.data.users[who].name || (async () => {
@@ -38,20 +19,17 @@ let handler = async (m, { conn, usedPrefix }) => {
   let user = global.db.data.users[who]
   let coin = user.coin || 0
   let bank = user.bank || 0
-  let total = (user.coin || 0) + (user.bank || 0)
+  let total = coin + bank
+  let currency = 'USD' // Define la moneda
 
-  let texto = `ᥫ᭡ Informacion - Balance ❀ ᰔᩚ Usuario » *${name}* ⛀ Cartera » *¥${coin.toLocaleString()} ${currency}* ⚿ Banco » *¥${bank.toLocaleString()} ${currency}* ⛁ Total » *¥${total.toLocaleString()} ${currency}* > *Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!*`
-      
-     await conn.sendMessage(m.chat, {
-      video: { url: video },
-      caption: texto,
-      gifPlayback: true,
-      mimetype: 'video/mp4'
-    }, { quoted: m })
-  } catch (error) {
-    console.log(error)
-    m.reply('Error al enviar el mensaje')
-  }
+  const texto = `ᥫ᭡ Informacion - Balance ❀ ᰔᩚ Usuario » *${name}* ⛀ Cartera » *${coin.toLocaleString()} ${currency}* ⚿ Banco » *${bank.toLocaleString()} ${currency}* ⛁ Total » *${total.toLocaleString()} ${currency}* > *Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!*`
+
+  await conn.sendMessage(m.chat, {
+    video: { url: 'https://files.catbox.moe/67rrf3.mp4' },
+    caption: texto,
+    gifPlayback: true,
+    mimetype: 'video/mp4'
+  }, { quoted: m })
 }
 
 handler.help = ['bal']
