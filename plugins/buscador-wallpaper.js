82d927c1af5ea6da66b21329c555a2635e0150cc
dv_shadow 🌿 <lopezacaro09@gmail.com>
2025-11-11 18:07:21 -0500

    Crear buscador-wallpaper.js

---
+++ plugins/buscador-wallpaper.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text)
+    return m.reply(`✳️ Ejemplo de uso:\n${usedPrefix + command} Naruto`)
+
+  try {
+    let res = await fetch(`https://xrljosedevapi.vercel.app/search/wallpaper?query=${encodeURIComponent(text)}`)
+    let data = await res.json()
+
+    if (!data.status || !data.data?.length)
+      return m.reply(`No se encontraron wallpapers para "${text}"`)
+
+    // Selecciona un wallpaper aleatorio
+    let result = data.data[Math.floor(Math.random() * data.data.length)]
+
+    let txt = `
+🎨 *Wallpaper encontrado* 🌲
+🪴 *Título:* ${result.title || "Sin título"}
+
+
+🪺 *Descargar:* 
+${result.downloadUrl}
+`
+
+    await conn.sendFile(
+      m.chat,
+      result.previewUrl || result.imageUrl,
+      'wallpaper.jpg',
+      txt,
+      m
+    )
+
+  } catch (e) {
+    console.error(e)
+    m.reply(" Ocurrió un error al buscar wallpapers.")
+  }
+}
+
+handler.help = ['wallpaper <texto>']
+handler.tags = ['internet', 'img']
+handler.command = ['wallpaper', 'fondos', 'wall']
+
+export default handler
