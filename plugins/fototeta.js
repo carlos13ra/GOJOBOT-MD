dfd2e382db2de78287ae13d2056e4f2a862d9cc1
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-11-15 14:41:06 -0500

    Update fototeta.js

--- plugins/fototeta.js
+++ plugins/fototeta.js
@@ -1,11 +1,9 @@
 import fetch from 'node-fetch';
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-
-m.react('🥵');
-
-m.react('✔️');
-conn.sendMessage(m.chat, Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal., estilo)
+  m.react('🥵');
+  m.react('✔️');
+  conn.sendMessage(m.chat, { text: 'Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.' });
 }
 
 handler.help = ['fototeta'];
