ccf297b68a091d36c8b7de1a5f5edc3861082cfe
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2026-01-07 17:16:53 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -73,26 +73,26 @@ ${ucapan()} @${userId.split('@')[0]}
 ────────────────
 👤 INFORMACIÓN DEL USUARIO
 ────────────────
-👤 Usuario: ${name}
-💎 Nivel: ${level}
-🗿 Experiencia: ${exp}
-🥵 Rango: Cachud@
+> 👤 Usuario: ${name}
+> 💎 Nivel: ${level}
+> 🗿 Experiencia: ${exp}
+> 🥵 Rango: Cachud@
 
 ────────────────
 🤖 INFORMACIÓN DEL BOT
 ────────────────
-🥭 Owner: wa.me/${suittag}
-🎧 Estado: ${(conn.user.jid == global.conn.user.jid ? 'BOT OFICIAL' : 'SUB BOT')}
-🎉 Comandos: ${totalCommands}
-👥 Usuarios: ${totalreg}
-⏳ Uptime: ${uptime}
+> 🥭 Owner: wa.me/${suittag}
+> 🎧 Estado: ${(conn.user.jid == global.conn.user.jid ? 'BOT OFICIAL' : 'SUB BOT')}
+> 🎉 Comandos: ${totalCommands}
+> 👥 Usuarios: ${totalreg}
+> ⏳ Uptime: ${uptime}
 
 ────────────────
 ⏰ FECHA Y HORA
 ────────────────
-🕝 Hora: ${hora}
-📅 Fecha: ${fecha}
-🏙️ Día: ${dia}
+> 🕝 Hora: ${hora}
+> 📅 Fecha: ${fecha}
+> 🏙️ Día: ${dia}
 ────────────────
 GOJO BOT • SISTEMA ACTIVO
 © 2025 - 2026 Powered By Carlos Ramírez
