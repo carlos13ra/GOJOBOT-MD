d458d3fff94c38d988fa75bb9e2d2d5e70134bc8
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-04-13 21:31:36 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const { chain } = lodash
 const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
-console.log(chalk.magentaBright('\n🌸 Iniciando...'))
+console.log(chalk.magentaBright('\n🌾 Iniciando...'))
 say('Gojobot-MD', {
 align: 'center',           
 gradient: ['red', 'blue'] 
