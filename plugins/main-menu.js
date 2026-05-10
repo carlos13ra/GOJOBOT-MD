69678ee26260f42fa6a1315ff2826a3c13841e1f
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2026-05-10 07:56:37 -0500

    Fix media URL formatting and add new video
    Removed extra space from a media URL and added a new media URL.

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -14,7 +14,8 @@ let handler = async (m, { conn, usedPrefix }) => {
     let fecha = fechaObj.toLocaleDateString('en-US', { timeZone: 'America/Lima' })
     let date = `${fecha} ${hora}`
     let mediaList = [
-      'https://files.catbox.moe/dd03ew.mp4 ',
+      'https://files.catbox.moe/dd03ew.mp4',
+      'https://files.catbox.moe/4pqpbt.mp4', 
       'https://files.catbox.moe/6o9e6r.mp4',
       'https://files.catbox.moe/b0toh8.mp4',
       'https://raw.githubusercontent.com/AkiraDevX/uploads/main/uploads/upfig.jpeg'
