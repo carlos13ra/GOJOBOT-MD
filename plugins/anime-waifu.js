3683988a1343b1cd28e4c519e4e35c5e5b026a10
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-05-24 22:00:36 -0500

    Actualizar anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -3,11 +3,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
 try {
 await m.react('🕒')
-let res = await fetch('https://api.waifu.pics/sfw/waifu')
-if (!res.ok) return
-let json = await res.json()
-if (!json.url) return
-await conn.sendFile(m.chat, json.url, 'thumbnail.jpg', '❀ Aquí tienes tu *Waifu* ฅ^•ﻌ•^ฅ.', fkontak)
+const api = 'https://api--shadowcorexyz.replit.app/random/waifu/v2'
+await conn.sendFile(m.chat, api, 'thumbnail.jpg', '🌱 Aquí tienes tu *Waifu* ฅ^•ﻌ•^ฅ.', fkontak)
 await m.react('✔️')
 } catch (error) {
 await m.react('✖️')
