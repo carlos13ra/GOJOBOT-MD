be16114e46f713b4846ce917f52aea4ca7687168
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-05-24 22:03:00 -0500

    Actualizar nsfw-pack.js

--- plugins/nsfw-pack.js
+++ plugins/nsfw-pack.js
@@ -1,30 +1,46 @@
 import fetch from 'node-fetch'
 
+const apis = [
+  'https://api.waifu.pics/nsfw/waifu',
+  'https://api.waifu.im/search?included_tags=waifu'
+]
+
 const handler = async (m, { conn }) => {
   try {
-    const res = await fetch('https://nekobot.xyz/api/image?type=waifu')
+    let image = null
+    try {
+      const res1 = await fetch(apis[0])
+      const json1 = await res1.json()
 
-    if (!res.ok) {
-      throw new Error('No se pudo obtener la imagen')
+      if (json1.url) {
+        image = json1.url
+      }
+    } catch {}
+    if (!image) {
+      try {
+        const res2 = await fetch(apis[1])
+        const json2 = await res2.json()
+
+        if (json2.images && json2.images.length > 0) {
+          image = json2.images[0].url
+        }
+      } catch {}
     }
 
-    const json = await res.json()
-
-    if (!json.message) {
-      throw new Error('La API no devolvió imagen')
+    if (!image) {
+      throw new Error('No se obtuvo ninguna imagen')
     }
 
     await conn.sendFile(
       m.chat,
-      json.message,
+      image,
       'pack.jpg',
-      '`Aquí tienes tu pack`',
+      '`Aquí tienes tu pack onichan 🥵`',
       m
     )
 
   } catch (error) {
     console.error(error)
-
     m.reply('✘ Ocurrió un error al obtener el pack.')
   }
 }
@@ -32,8 +48,7 @@ const handler = async (m, { conn }) => {
 handler.command = ['pack2']
 handler.tags = ['nsfw']
 handler.help = ['pack2']
-handler.level = 10
-handler.register = true
+handler.level = 50
 handler.premium = true
 
 export default handler
