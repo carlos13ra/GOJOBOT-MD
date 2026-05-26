a576d8414dc4ff04b923aa9c277e69c8d41ce95f
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-05-25 20:11:53 -0500

    Actualizar dl-scdl.js

--- plugins/dl-scdl.js
+++ plugins/dl-scdl.js
@@ -1,6 +1,26 @@
 import fetch from "node-fetch"
 import jimp from "jimp"
 
+const qualities = [
+  '64',
+  '96',
+  '128',
+  '160',
+  '192',
+  '224',
+  '256',
+  '320'
+]
+
+const fandoms = [
+  '🌱 ძᥱsᥴᥲrgᥲᥒძ᥆ ᥲᥙძі᥆ ᥴᥲᥙsᥲ...',
+  '🍃 ᥱs⍴ᥱrᥲ ᥙᥒ m᥆mᥱᥒ𝗍᥆ ᥴᥲᥙsᥲ...',
+  '🎧 ᥴᥲrgᥲᥒძ᥆ 𝗍ᥙ ᥲᥙძі᥆...',
+  '🌴 ᥴ᥆ᥒsіgᥙіᥱᥒძ᥆ ᥣᥲ mᥙsіᥴᥲ...',
+  '🍄 ძᥱsᥴᥲrgᥲ ᥱᥒ ⍴r᥆ᥴᥱs᥆...',
+  '💿 ᥆ᑲ𝗍ᥱᥒіᥱᥒძ᥆ ᥱᥣ ᥲᥙძі᥆...'
+]
+
 const handler = async (m, { conn, text }) => {
   try {
     if (!text) {
@@ -18,13 +38,36 @@ const handler = async (m, { conn, text }) => {
       )
     }
 
+    const quality =
+      qualities.find(v =>
+        text.includes(v)
+      ) ||
+      qualities[
+        Math.floor(
+          Math.random() *
+          qualities.length
+        )
+      ]
+
     await m.react("🕒")
-    m.reply(`*🌱 ძᥱsᥴᥲrgᥲᥒძ᥆ ᥲᥙძі᥆ ᥴᥲᥙsᥲ...*`)
+
+    const fandom =
+      fandoms[
+        Math.floor(
+          Math.random() *
+          fandoms.length
+        )
+      ]
+
+    m.reply(`*${fandom}*`)
+
     const api =
-      `https://api--shadowcorexyz.replit.app/download/soundcloud/v2?url=${encodeURIComponent(text)}&quality=128`
+      `https://api--shadowcorexyz.replit.app/download/soundcloud/v2?url=${encodeURIComponent(text)}&quality=${quality}`
 
     const res = await fetch(api)
+
     const json = await res.json()
+
     if (!json?.status || !json?.result) {
       throw new Error(
         "No se obtuvo resultado"
@@ -32,6 +75,7 @@ const handler = async (m, { conn, text }) => {
     }
 
     const data = json.result
+
     let thumbDoc = null
 
     try {
@@ -64,18 +108,30 @@ const handler = async (m, { conn, text }) => {
 > 👁️ *Views:* ${data.views}
 > ❤️ *Likes:* ${data.likes}
 > 📅 *Upload:* ${data.upload}
-> 💿 *Calidad:* ${data.quality}
+> 💿 *Calidad:* ${quality}kbps
 > 📦 *Peso:* ${data.size}
 > 🎵 *Formato:* ${data.format}
 > 🔗 *Source:* ${data.source}`
-    await conn.sendMessage(m.chat, {
-       document: { url: data.download },
-       mimetype: "audio/mpeg",
-       fileName: data.filename || `${data.title}.mp3`,
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        document: {
+          url: data.download
+        },
+
+        mimetype: "audio/mpeg",
+
+        fileName:
+          data.filename ||
+          `${data.title}.mp3`,
+
         caption,
+
         jpegThumbnail: thumbDoc
-    },
-    { quoted: m })
+      },
+      { quoted: m }
+    )
 
     await m.react("✅")
 
