667d1f7a2417640d1b8e7fce0fbc5029f41bb39b
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-05-25 21:53:38 -0500

    Actualizar main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -2,16 +2,7 @@ import speed from 'performance-now'
 import os from 'os'
 
 let handler = async (m, { conn }) => {
-  const start = speed()
-  await new Promise(res => setTimeout(res, 1))
-
-  const latensi = speed() - start
-
-  let ramTotal = (os.totalmem() / 1024 / 1024).toFixed(0)
-  let ramLibre = (os.freemem() / 1024 / 1024).toFixed(0)
-  let ramUso = ramTotal - ramLibre
-
-  let uptime = process.uptime()
+  const start = Date.now()
 
   const fakePaymentQuote = {
     key: {
@@ -41,6 +32,16 @@ let handler = async (m, { conn }) => {
     participant: m.sender
   }
 
+  await conn.reply(m.chat, '`🌱 ¡Pong!`', fakePaymentQuote, fake)
+
+  const latensi = Date.now() - start
+
+  let ramTotal = (os.totalmem() / 1024 / 1024).toFixed(0)
+  let ramLibre = (os.freemem() / 1024 / 1024).toFixed(0)
+  let ramUso = ramTotal - ramLibre
+
+  let uptime = process.uptime()
+
   let pingEmoji =
     latensi < 50 ? '🟢 Excelente' :
     latensi < 120 ? '🟡 Bueno' :
@@ -49,14 +50,12 @@ let handler = async (m, { conn }) => {
 
   let teks = `*'ׄ𐚁ִㅤS T A T U S - P I N Gׄ ₍ ᐢ..ᐢ ₎'*
 
-*🌳 Latency   : ›* ${latensi.toFixed(2)} ms (${pingEmoji})
+*🌳 Latency   : ›* ${latensi} ms (${pingEmoji})
 *🍄 Bot       : ›* ${botname}
 *🌱 Uptime    : ›* ${formatTime(uptime)}
 *🪷 Sistema   : ›* ${os.platform()} (${os.arch()})
 *🍙 Node      : ›* ${process.version}
 *🌿 RAM       : ›* ${ramUso} MB / ${ramTotal} MB`
-
-  await conn.reply(m.chat, '`🌱 ¡Pong!`', fakePaymentQuote, fake)
 
   await conn.reply(m.chat, teks, fakePaymentQuote, fake)
 }
@@ -64,10 +63,13 @@ let handler = async (m, { conn }) => {
 handler.help = ['ping']
 handler.tags = ['info']
 handler.command = ['ping', 'p']
+handler.register = true
+
 export default handler
 
 function formatTime(seconds) {
   seconds = Number(seconds)
+
   let d = Math.floor(seconds / (3600 * 24))
   let h = Math.floor(seconds % (3600 * 24) / 3600)
   let m = Math.floor(seconds % 3600 / 60)
