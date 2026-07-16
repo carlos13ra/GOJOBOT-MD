a273873d4944426fd9d2954b3d8e4a7812b8b002
Dev-lxyz <lopezacaro09@gmail.com>
2026-07-15 20:17:09 -0500

    Update

--- plugins/_test.js
+++ plugins/_test.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url'
 import ytSearch from 'yt-search'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const downloadDir = path.join(__dirname, '..', '..', 'tmp')
+const downloadDir = path.join(__dirname, '..', 'tmp')
 
 if (!fs.existsSync(downloadDir)) {
   fs.mkdirSync(downloadDir, { recursive: true })
