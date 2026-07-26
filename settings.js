bd78ae47c453f4db55d32386c834a795cca3981b
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-07-26 11:57:55 -0500

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ import fs from "fs"
 import { prepareWAMessageMedia } from '@whiskeysockets/baileys'
 
 global.botNumber = "" 
-global.owner = ["51963315293", "51934053286", "51978385249", "51972409783", "51966453839", "5213541145561"]
+global.owner = ["51963315293", "51934053286", "51978385249", "51922902497", "51966453839", "5213541145561"]
 global.suittag = ["51963315293"] 
 global.prems = []
 
