92c8f96d82dc043af3fc5b05f564e2b81d8354da
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-07-26 14:11:12 -0500

    Actualizar dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -48,7 +48,7 @@ let handler = async (m, { conn, text, command }) => {
         }
       }, { quoted: m })
 
-    const dlRes = await fetch(`${global.APIs.light2.url}/download/savetube?url=${encodeURIComponent(video.url)}&type=video`)
+    const dlRes = await fetch(`${global.APIs.light.url}/download/savetube?url=${encodeURIComponent(video.url)}&type=video`)
     const dlJson = await dlRes.json()
 
     if (!dlJson.status || !dlJson.data?.dl)
