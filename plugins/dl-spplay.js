4cedcd3cd68d3132ee7ee17d57ebff71ac236990
𐚁 ɪ'ᴍ ꭛sʜᴀᴅᴏᴡ ꈍꈍ <lopezacaro09@gmail.com>
2026-09-16 19:24:20 -0500

    Actualizar dl-spplay.js

--- plugins/dl-spplay.js
+++ plugins/dl-spplay.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch'
+import Jimp from 'jimp'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
@@ -6,22 +7,16 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       return m.reply(`《✧》 Ingresa el nombre de la canción\n\nEjemplo: ${usedPrefix}${command} goth slowed`)
     }
 
-    const quality = text.includes('320') ? '320' : '128'
-    const query = text.replace(/320|128/g, '').trim()
-
-    await m.react('🔍')
-
     const res = await fetch(
-      `${global.APIs.light.url}/api/play/spotify?q=${encodeURIComponent(query)}&quality=${quality}`
+      `${global.APIs.light.url}/api/play/spotify?q=${encodeURIComponent(text)}`
     )
     const data = await res.json()
 
-    if (!data.status || !data.shadow || data.shadow.length === 0) {
-      await m.react('❌')
+    if (!data.status || !data.result) {
       return m.reply('《✧》 No se encontraron canciones')
     }
 
-    const track = data.shadow[0]
+    const track = data.result
 
     const info = `╔═══════ ≪ - ≫ ═══════╗
 ║ 🍡 *SPOTIFY - MUSIC*
@@ -30,30 +25,37 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 ║ ° *Artista:* ${track.artist}
 ║ ° *Álbum:* ${track.album}
 ║ ° *Duración:* ${track.duration}
-║ ° *Calidad:* ${track.bitrate}
 ║ ° *Publicado:* ${track.publish}
 ║ ° *Tamaño:* ${track.size}
-║ ° *Link:* ${track.url}
+║ ° *Link:* ${track.link}
 ╚═══════ ≪ - ≫ ═══════╝`
 
-    await conn.sendFile(m.chat, track.image, 'spotify.jpg', info, m)
-    
-    await m.react('⏳')
-    await conn.sendMessage(m.chat, { 
-     audio: { url: track.dl },
-     mimetype: 'audio/mpeg',
-     fileName: `${track.title}.mp3` },
-   { quoted: m })
-    
-    await m.react('✅')
+    await conn.sendFile(m.chat, track.image, 'spotify.jpg', info, m, fake)
+    let thumbDoc = null;
+    try {
+      const img = await Jimp.read(track.image);
+      img.resize(300, Jimp.AUTO).quality(70);
+      thumbDoc = await img.getBufferAsync(Jimp.MIME_JPEG);
+    } catch (err) {
+      console.log("⚠️ Error al procesar miniatura:", err.message);
+      thumbDoc = Buffer.alloc(0);
+    }
 
+    await conn.sendMessage(m.chat, { 
+      document: { url: track.download },
+      mimetype: 'audio/mpeg',
+      fileName: `${track.title}.mp3`,
+      jpegThumbnail: thumbDoc
+    }, { quoted: m })
+    
   } catch (e) {
     m.reply(`《✧》 Error: \`\`\`${e.message}\`\`\``)
   }
 }
 
-handler.help = ['spotifyplay <canción> [128|320]', 'spplay <canción> [128|320]']
-handler.tags = ['downloader', 'music']
-handler.command = ['spotifyplay', 'spplay', 'song', 'music']
+handler.help = ['spotifyplay <canción>', 'spplay <canción>']
+handler.tags = ['music']
+handler.command = ['spotifyplay', 'spplay']
+handler.limit = true
 
 export default handler
